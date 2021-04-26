096d6da24a369276a4a7658c84bd21d3f0b62439
ivann0 <ivanradulovic.s1a@gmail.com>
2021-04-27 00:22:20 +0200

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'ucenik');
+  define('DB_PASS', '1234');
+  define('DB_NAME', 'ucenik');
+
+  define('APPROOT', dirname(dirname(__FILE__)));
+  define('URLROOT', 'http://37.0.71.52/s4f.test/Ivan.Radulovic/shareposts');
+  define('SITENAME', 'SharePosts');
+  define('APPVERSION', '1.0.0');
